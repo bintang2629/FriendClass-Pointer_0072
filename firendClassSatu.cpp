39033e02238df834d14f710681669f63246af26f
bintang2629 <bimbimpratama23@gmail.com>
2026-04-28 15:29:05 +0700

    setNama

---
+++ firendClassSatu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+    
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+
